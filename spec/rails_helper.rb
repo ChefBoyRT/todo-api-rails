af7c4697a57add48a4e0348ad596e8bc306eb8dc
Taylor <stein0209@gmail.com>
2019-11-05 16:45:19 -0700

    removes comments for DatabaseCleaner setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 require 'database_cleaner'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -74,21 +75,16 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
-  # [...]
-  # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
 
-  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
   end
 
-    # start the transaction strategy as examples are run
-    config.around(:each) do |example|
-      DatabaseCleaner.cleaning do
-        example.run
-      end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
     end
-    # [...]
   end
+end
