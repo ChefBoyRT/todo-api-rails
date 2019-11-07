2be82c8e54cf1c5292a1e3d4120701e2b3b9a834
Taylor <stein0209@gmail.com>
2019-11-07 10:07:54 -0700

    updates items and todos controller to v1 module

--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -1,42 +1,44 @@
-class TodosController < ApplicationController
-    before_action :set_todo, only: [:show, :update, :destroy]
+module v1
+    class TodosController < ApplicationController
+        before_action :set_todo, only: [:show, :update, :destroy]
 
-    def index
-        @todos = current_user.todos
+        def index
+            @todos = current_user.todos
 
-        json_response(@todos)
-    end
+            json_response(@todos)
+        end
 
-    def create
-        @todo = current_user.todos.create!(todo_params)
-    
-        json_response(@todo, :created)
-    end
-
-    def show
-        json_response(@todo)
-    end
-    
-    def update
-        @todo.update(todo_params)
+        def create
+            @todo = current_user.todos.create!(todo_params)
         
-        head :no_content
-    end
+            json_response(@todo, :created)
+        end
 
-    def destroy
-        @todo.destroy
+        def show
+            json_response(@todo)
+        end
         
-        head :no_content
+        def update
+            @todo.update(todo_params)
+            
+            head :no_content
+        end
+
+        def destroy
+            @todo.destroy
+            
+            head :no_content
+        end
+
+        private
+
+        def todo_params
+            params.permit(:title)
+        end
+
+        def set_todo
+            @todo = Todo.find(params[:id])
+        end
+
     end
-
-    private
-
-    def todo_params
-        params.permit(:title)
-    end
-
-    def set_todo
-        @todo = Todo.find(params[:id])
-    end
-
 end
